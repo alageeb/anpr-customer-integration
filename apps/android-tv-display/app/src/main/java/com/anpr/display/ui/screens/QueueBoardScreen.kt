fe3905b1346b7fd8b75067e91ea09b849b9d13da
alageeb <ah.yousef79@gmail.com>
2026-07-15 14:29:36 +0300

    Fix QueueBoardScreen: add missing sp import

--- apps/android-tv-display/app/src/main/java/com/anpr/display/ui/screens/QueueBoardScreen.kt
+++ apps/android-tv-display/app/src/main/java/com/anpr/display/ui/screens/QueueBoardScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.anpr.display.data.model.*
 import com.anpr.display.ui.theme.*
 import kotlinx.coroutines.delay
